73dbb1ae46de99b6b02176f6bca00acefb442775
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-10-17 21:16:39 -0500

    Removing back ticks on column addition section
    Removing in case it is the source of the undefined column section.

--- Scripts/Analysis_Plot_Groups_proj_onlyEnvs_FullExtent.R
+++ Scripts/Analysis_Plot_Groups_proj_onlyEnvs_FullExtent.R
@@ -175,11 +175,11 @@ Prec_of_Wettest_Month_2041_2070_585_df <- Prec_of_Wettest_Month_2041_2070_585_df
 # 2011-2040 ssp126
 MRIESM2_1140_126_Stats <- MRIESM2_1140_126_Avg_df
 MRIESM2_1140_126_Stats <- MRIESM2_1140_126_Stats %>% distinct()
-MRIESM2_1140_126_Stats$`Annual_Mean_Temp_2011_2040_126` <- Annual_Mean_Temp_2011_2040_126_df$`Annual_Mean_Temp_2011_2040_126`
-MRIESM2_1140_126_Stats$`Mean_Diurnal_Range_2011_2040_126` <- Mean_Diurnal_Range_2011_2040_126_df$`Mean_Diurnal_Range_2011_2040_126`
-MRIESM2_1140_126_Stats$`Isothermality_2011_2040_126` <- Isothermality_2011_2040_126_df$`Isothermality_2011_2040_126`
-MRIESM2_1140_126_Stats$`Mean_Temp_Driest_Qtr_2011_2040_126` <- Mean_Temp_Driest_Qtr_2011_2040_126_df$`Mean_Temp_Driest_Qtr_2011_2040_126`
-MRIESM2_1140_126_Stats$`Prec_of_Wettest_Month_2011_2040_126` <- Prec_of_Wettest_Month_2011_2040_126_df$`Prec_of_Wettest_Month_2011_2040_126`
+MRIESM2_1140_126_Stats$Annual_Mean_Temp_2011_2040_126 <- Annual_Mean_Temp_2011_2040_126_df$Annual_Mean_Temp_2011_2040_126
+MRIESM2_1140_126_Stats$Mean_Diurnal_Range_2011_2040_126 <- Mean_Diurnal_Range_2011_2040_126_df$Mean_Diurnal_Range_2011_2040_126
+MRIESM2_1140_126_Stats$Isothermality_2011_2040_126 <- Isothermality_2011_2040_126_df$Isothermality_2011_2040_126
+MRIESM2_1140_126_Stats$Mean_Temp_Driest_Qtr_2011_2040_126 <- Mean_Temp_Driest_Qtr_2011_2040_126_df$Mean_Temp_Driest_Qtr_2011_2040_126
+MRIESM2_1140_126_Stats$Prec_of_Wettest_Month_2011_2040_126 <- Prec_of_Wettest_Month_2011_2040_126_df$Prec_of_Wettest_Month_2011_2040_126
 
 # Rename
 rename(MRIESM2_1140_126_Stats, 'Predicted_Bd_Suitability' = 'Predicted Bd Suitability')
@@ -187,11 +187,11 @@ rename(MRIESM2_1140_126_Stats, 'Predicted_Bd_Suitability' = 'Predicted Bd Suitab
 # 2011-2040 ssp585
 MRIESM2_1140_585_Stats <- MRIESM2_1140_585_Avg_df
 MRIESM2_1140_585_Stats <- MRIESM2_1140_126_Stats %>% distinct()
-MRIESM2_1140_585_Stats$`Annual_Mean_Temp_2011_2040_585` <- Annual_Mean_Temp_2011_2040_585_df$`Annual_Mean_Temp_2011_2040_585`
-MRIESM2_1140_585_Stats$`Mean_Diurnal_Range_2011_2040_585` <- Mean_Diurnal_Range_2011_2040_585_df$`Mean_Diurnal_Range_2011_2040_585`
-MRIESM2_1140_585_Stats$`Isothermality_2011_2040_585` <- Isothermality_2011_2040_585_df$`Isothermality_2011_2040_585`
-MRIESM2_1140_585_Stats$`Mean_Temp_Driest_Qtr_2011_2040_585` <- Mean_Temp_Driest_Qtr_2011_2040_585_df$`Mean_Temp_Driest_Qtr_2011_2040_585`
-MRIESM2_1140_585_Stats$`Prec_of_Wettest_Month_2011_2040_585` <- Prec_of_Wettest_Month_2011_2040_585_df$`Prec_of_Wettest_Month_2011_2040_585`
+MRIESM2_1140_585_Stats$Annual_Mean_Temp_2011_2040_585 <- Annual_Mean_Temp_2011_2040_585_df$Annual_Mean_Temp_2011_2040_585
+MRIESM2_1140_585_Stats$Mean_Diurnal_Range_2011_2040_585 <- Mean_Diurnal_Range_2011_2040_585_df$Mean_Diurnal_Range_2011_2040_585
+MRIESM2_1140_585_Stats$Isothermality_2011_2040_585 <- Isothermality_2011_2040_585_df$Isothermality_2011_2040_585
+MRIESM2_1140_585_Stats$Mean_Temp_Driest_Qtr_2011_2040_585 <- Mean_Temp_Driest_Qtr_2011_2040_585_df$Mean_Temp_Driest_Qtr_2011_2040_585
+MRIESM2_1140_585_Stats$Prec_of_Wettest_Month_2011_2040_585 <- Prec_of_Wettest_Month_2011_2040_585_df$Prec_of_Wettest_Month_2011_2040_585
 
 # Rename
 rename(MRIESM2_1140_585_Stats, 'Predicted_Bd_Suitability' = 'Predicted Bd Suitability')
@@ -199,11 +199,11 @@ rename(MRIESM2_1140_585_Stats, 'Predicted_Bd_Suitability' = 'Predicted Bd Suitab
 # 2041-2070 ssp126
 MRIESM2_4170_126_Stats <- MRIESM2_4170_126_Avg_df
 MRIESM2_4170_126_Stats <- MRIESM2_1140_126_Stats %>% distinct()
-MRIESM2_4170_126_Stats$`Annual_Mean_Temp_2041_2070_126` <- Annual_Mean_Temp_2041_2070_126_df$`Annual_Mean_Temp_2041_2070_126`
-MRIESM2_4170_126_Stats$`Mean_Diurnal_Range_2041_2070_126` <- Mean_Diurnal_Range_2041_2070_126_df$`Mean_Diurnal_Range_2041_2070_126`
-MRIESM2_4170_126_Stats$`Isothermality_2041_2070_126` <- Isothermality_2041_2070_126_df$`Isothermality_2041_2070_126`
-MRIESM2_4170_126_Stats$`Mean_Temp_Driest_Qtr_2041_2070_126` <- Mean_Temp_Driest_Qtr_2041_2070_126_df$`Mean_Temp_Driest_Qtr_2041_2070_126`
-MRIESM2_4170_126_Stats$`Prec_of_Wettest_Month_2041_2070_126` <- Prec_of_Wettest_Month_2041_2070_126_df$`Prec_of_Wettest_Month_2041_2070_126`
+MRIESM2_4170_126_Stats$Annual_Mean_Temp_2041_2070_126 <- Annual_Mean_Temp_2041_2070_126_df$Annual_Mean_Temp_2041_2070_126
+MRIESM2_4170_126_Stats$Mean_Diurnal_Range_2041_2070_126 <- Mean_Diurnal_Range_2041_2070_126_df$Mean_Diurnal_Range_2041_2070_126
+MRIESM2_4170_126_Stats$Isothermality_2041_2070_126 <- Isothermality_2041_2070_126_df$Isothermality_2041_2070_126
+MRIESM2_4170_126_Stats$Mean_Temp_Driest_Qtr_2041_2070_126 <- Mean_Temp_Driest_Qtr_2041_2070_126_df$Mean_Temp_Driest_Qtr_2041_2070_126
+MRIESM2_4170_126_Stats$Prec_of_Wettest_Month_2041_2070_126 <- Prec_of_Wettest_Month_2041_2070_126_df$Prec_of_Wettest_Month_2041_2070_126
 
 # Rename
 rename(MRIESM2_4170_126_Stats, 'Predicted_Bd_Suitability' = 'Predicted Bd Suitability')
@@ -211,11 +211,11 @@ rename(MRIESM2_4170_126_Stats, 'Predicted_Bd_Suitability' = 'Predicted Bd Suitab
 # 2041-2070 ssp585
 MRIESM2_4170_585_Stats <- MRIESM2_4170_585_Avg_df
 MRIESM2_4170_585_Stats <- MRIESM2_4170_585_Stats %>% distinct()
-MRIESM2_4170_585_Stats$`Annual_Mean_Temp_2041_2070_585` <- Annual_Mean_Temp_2041_2070_585_df$`Annual_Mean_Temp_2041_2070_585`
-MRIESM2_4170_585_Stats$`Mean_Diurnal_Range_2041_2070_585` <- Mean_Diurnal_Range_2041_2070_585_df$`Mean_Diurnal_Range_2041_2070_585`
-MRIESM2_4170_585_Stats$`Isothermality_2041_2070_585` <- Isothermality_2041_2070_585_df$`Isothermality_2041_2070_585`
-MRIESM2_4170_585_Stats$`Mean_Temp_Driest_Qtr_2041_2070_585` <- Mean_Temp_Driest_Qtr_2041_2070_585_df$`Mean_Temp_Driest_Qtr_2041_2070_585`
-MRIESM2_4170_585_Stats$`Prec_of_Wettest_Month_2041_2070_585` <- Prec_of_Wettest_Month_2041_2070_585_df$`Prec_of_Wettest_Month_2041_2070_585`
+MRIESM2_4170_585_Stats$Annual_Mean_Temp_2041_2070_585 <- Annual_Mean_Temp_2041_2070_585_df$Annual_Mean_Temp_2041_2070_585
+MRIESM2_4170_585_Stats$Mean_Diurnal_Range_2041_2070_585 <- Mean_Diurnal_Range_2041_2070_585_df$Mean_Diurnal_Range_2041_2070_585
+MRIESM2_4170_585_Stats$Isothermality_2041_2070_585 <- Isothermality_2041_2070_585_df$Isothermality_2041_2070_585
+MRIESM2_4170_585_Stats$Mean_Temp_Driest_Qtr_2041_2070_585 <- Mean_Temp_Driest_Qtr_2041_2070_585_df$Mean_Temp_Driest_Qtr_2041_2070_585
+MRIESM2_4170_585_Stats$Prec_of_Wettest_Month_2041_2070_585 <- Prec_of_Wettest_Month_2041_2070_585_df$Prec_of_Wettest_Month_2041_2070_585
 
 # Rename
 rename(MRIESM2_4170_585_Stats, 'Predicted_Bd_Suitability' = 'Predicted Bd Suitability')
