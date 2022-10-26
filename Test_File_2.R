8dad2617ad7c119c209e517328df3dbd0f22ad6d
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-10-25 20:59:17 -0500

    Update Test_File_2.R

--- Test_File_2.R
+++ Test_File_2.R
@@ -10,7 +10,7 @@ Annual_Mean_Temp_1140_126_Queensland_df <- Annual_Mean_Temp_2011_2040_126_df %>%
     filter(between(Longitude, 138.00, 155.00) | between(Latitude, -29.00, -9.00))
 
 Annual_Mean_Temp_1140_126_Qnslnd_Map <- ggplot(data = Annual_Mean_Temp_1140_126_Queensland_df, aes(x=Longitude, y=Latitude)) +
-  geom_raster(aes(fill = `Predicted_Bd_Suitability`)) +
+  geom_raster(aes(fill = `Annual_Mean_Temp_2011_2040_126`)) +
   labs(x = "Longitude", y = "Latitude") +
   coord_sf(xlim = c(138.00, 155.00), ylim = c(-29.00, -9.00), expand = T, crs = "WGS84") +
   annotation_scale(location = "bl", width_hint = 0.5) +
