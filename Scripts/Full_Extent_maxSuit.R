a76e2538f581a586bcccc180fd5c83bda6f9a978
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-11-04 21:26:34 -0500

    comma fix

--- Scripts/Full_Extent_maxSuit.R
+++ Scripts/Full_Extent_maxSuit.R
@@ -99,8 +99,8 @@ stat_compare_means(comparisons = list(c('(2011-2040) ssp126',
                                       c('(2011-2040) ssp585',
                                         '(2041-2070) ssp585'),
                                       c('(2011-2040) ssp126',
-                                        '(2041-2070) ssp585'))
-                  label = "p.signif", hide.ns = FALSE) +
+                                        '(2041-2070) ssp585')),
+                   label = "p.signif", hide.ns = FALSE) +
 stat_compare_means(label.y = 50)
 
 
@@ -150,7 +150,7 @@ stat_compare_means(comparisons = list(c('(2011-2040) ssp126',
                                       c('(2011-2040) ssp585',
                                         '(2041-2070) ssp585'),
                                       c('(2011-2040) ssp126',
-                                        '(2041-2070) ssp585'))
+                                        '(2041-2070) ssp585')),
                   label = "p.signif", hide.ns = FALSE) +
 stat_compare_means(label.y = 35)
 
@@ -201,7 +201,7 @@ stat_compare_means(comparisons = list(c('(2011-2040) ssp126',
                                       c('(2011-2040) ssp585',
                                         '(2041-2070) ssp585'),
                                       c('(2011-2040) ssp126',
-                                        '(2041-2070) ssp585'))
+                                        '(2041-2070) ssp585')),
                   label = "p.signif", hide.ns = FALSE) +
 stat_compare_means(label.y = 1.5)
 
@@ -251,7 +251,7 @@ stat_compare_means(comparisons = list(c('(2011-2040) ssp126',
                                       c('(2011-2040) ssp585',
                                         '(2041-2070) ssp585'),
                                       c('(2011-2040) ssp126',
-                                        '(2041-2070) ssp585'))
+                                        '(2041-2070) ssp585')),
                   label = "p.signif", hide.ns = FALSE) +
 stat_compare_means(label.y = 50)
 
@@ -302,6 +302,6 @@ stat_compare_means(comparisons = list(c('(2011-2040) ssp126',
                                       c('(2011-2040) ssp585',
                                         '(2041-2070) ssp585'),
                                       c('(2011-2040) ssp126',
-                                        '(2041-2070) ssp585'))
+                                        '(2041-2070) ssp585')),
                   label = "p.signif", hide.ns = FALSE) +
 stat_compare_means(label.y = 550)
