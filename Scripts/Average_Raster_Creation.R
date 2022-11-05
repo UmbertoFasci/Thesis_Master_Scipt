18f7f5fa6a865d888cecc0b3fc2ac060db67222a
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-11-05 13:19:26 -0500

    initialize current analysis

--- Scripts/Average_Raster_Creation.R
+++ Scripts/Average_Raster_Creation.R
@@ -1,6 +1,10 @@
 library(raster)
 library(tidyverse)
 
+# Current
+Current_1k_var1_ras <- raster("/Users/umbertofasci/Thesis_Master_Final/Thesis_Master_Data/B_Dendrobatidis_Models/Current/Current_1k_vargroup1/Batrachochytrium_dendrobatidis.asc")
+Current_1k_var5_ras <- raster("/Users/umbertofasci/Thesis_Master_Final/Thesis_Master_Data/B_Dendrobatidis_Models/Current/Current_1k_vargroup5/Batrachochytrium_dendrobatidis.asc")
+
 # 2011-2040 ssp126
 MRIESM2_1140_126_10k_var6_ras <- raster("/Users/umbertofasci/Thesis_Master_Final/Thesis_Master_Data/B_Dendrobatidis_Models/MRIESM2_1140/ssp126/MRIESM2_1140_ssp126_10k_vargroup6/Batrachochytrium_dendrobatidis.asc")
 MRIESM2_1140_126_1k_var1_ras <- raster("/Users/umbertofasci/Thesis_Master_Final/Thesis_Master_Data/B_Dendrobatidis_Models/MRIESM2_1140/ssp126/MRIESM2_1140_ssp126_1k_vargroup1/Batrachochytrium_dendrobatidis.asc")
@@ -19,6 +23,9 @@ MRIESM2_4170_585_10k_var6_ras <- raster("/Users/umbertofasci/Thesis_Master_Final
 MRIESM2_4170_585_1k_var1_ras <- raster("/Users/umbertofasci/Thesis_Master_Final/Thesis_Master_Data/B_Dendrobatidis_Models/MRIESM2_4170/ssp585/MRIESM2_4170_ssp585_1k_vargroup1/Batrachochytrium_dendrobatidis.asc")
 
 # Average raster groups
+
+# Current
+Current_Avg <- (Current_1k_var1_ras + Current_1k_var5_ras) / 2
 
 # 2011-2040 ssp126
 MRIESM2_1140_126_Avg <- (MRIESM2_1140_126_10k_var6_ras + MRIESM2_1140_126_1k_var1_ras + MRIESM2_1140_126_1k_var5_ras) / 3
