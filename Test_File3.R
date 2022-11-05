0ce899af42b39b4e1dc8d4ce65cbfc1192ea32d8
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-11-04 20:30:09 -0500

    update

--- Test_File3.R
+++ Test_File3.R
@@ -1,3 +1,9 @@
+colnames(An_Mn_Tmp_full_mxst) <- c("(2011-2040) ssp126", "(2011-2040) ssp585",
+                                   "(2041-2070) ssp126", "(2041-2070) ssp585")
+
+An_Mn_Tmp_full_mxst <- An_Mn_Tmp_full_mxst %>%
+  pivot_longer(everything(), names_to = "Variable", values_to = "Value")
+
 An_Mn_Tmp_full_mxst_boxplot <- An_Mn_Tmp_full_mxst %>% 
   ggboxplot(x = "Variable",
   y = "Value",
@@ -5,5 +11,17 @@ An_Mn_Tmp_full_mxst_boxplot <- An_Mn_Tmp_full_mxst %>%
   ylab = "Annual Mean Temperature Values",
   fill = "Variable",
   palette = c("#2e00fa", "#a000bc", "#ca0086", "#e40058")) +
-  stat_compare_means(comparisons = bin_comparisons, label = "p.signif") +
-  stat_compare_means(label.y = 55)
+  stat_compare_means(comparisons = list(c('(2011-2040) ssp126',
+                                          '(2011-2040) ssp585'),
+                                        c('(2011-2040) ssp585',
+                                          '(2041-2070) ssp126'),
+                                        c('(2041-2070) ssp126',
+                                          '(2041-2070) ssp585'),
+                                        c('(2011-2040) ssp126',
+                                          '(2041-2070) ssp126'),
+                                        c('(2011-2040) ssp585',
+                                          '(2041-2070) ssp585'),
+                                        c('(2011-2040) ssp126',
+                                          '(2041-2070) ssp585')),
+                     label = "p.signif", hide.ns = FALSE) +
+  stat_compare_means(label.y = 50)
