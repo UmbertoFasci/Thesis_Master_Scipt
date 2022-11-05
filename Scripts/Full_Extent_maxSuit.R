fc0b4659a5b33d5317e3e29cc7218be4a030009b
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-11-04 21:23:09 -0500

    update label.y argument for stats

--- Scripts/Full_Extent_maxSuit.R
+++ Scripts/Full_Extent_maxSuit.R
@@ -152,7 +152,7 @@ stat_compare_means(comparisons = list(c('(2011-2040) ssp126',
                                       c('(2011-2040) ssp126',
                                         '(2041-2070) ssp585'))
                   label = "p.signif", hide.ns = FALSE) +
-stat_compare_means(label.y = 50)
+stat_compare_means(label.y = 35)
 
 
 # Isothermality
@@ -203,7 +203,7 @@ stat_compare_means(comparisons = list(c('(2011-2040) ssp126',
                                       c('(2011-2040) ssp126',
                                         '(2041-2070) ssp585'))
                   label = "p.signif", hide.ns = FALSE) +
-stat_compare_means(label.y = 50)
+stat_compare_means(label.y = 1.5)
 
 # Mean Temperature of the Driest Quarter
 
@@ -304,4 +304,4 @@ stat_compare_means(comparisons = list(c('(2011-2040) ssp126',
                                       c('(2011-2040) ssp126',
                                         '(2041-2070) ssp585'))
                   label = "p.signif", hide.ns = FALSE) +
-stat_compare_means(label.y = 50)
+stat_compare_means(label.y = 550)
