2c6437106ab9d24596d9084a0fe4093fc35dffa7
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-11-06 22:01:24 -0600

    PrcWt Current Map

--- Scripts/Analysis_Plot_Groups_proj_onlyEnvs_FullExtent.R
+++ Scripts/Analysis_Plot_Groups_proj_onlyEnvs_FullExtent.R
@@ -749,6 +749,16 @@ Mean_Temp_Driest_Qtr_2041_2070_585_map <- ggplot(data = Mean_Temp_Driest_Qtr_204
 
 # Precipitation of The Wettest Month
 
+Prec_of_Wettest_Month_Current_map <- ggplot(data = Prec_of_Wettest_Month_Curr_df, aes(x=Longitude, y=Latitude)) +
+  geom_tile(aes(fill = `Prec_of_Wettest_Month_Current`)) +
+  labs(x = "Longitude", y = "Latitude") +
+  coord_sf(xlim = c(110.00, 160.00), ylim = c(-45.00, -10.00), expand = T, crs = "WGS84") +
+  annotation_scale(location = "bl", width_hint = 0.5) +
+  theme_bw() +
+  theme(legend.title = element_blank(),
+        legend.key.size = unit(6, "mm"), legend.text = element_text(size = 6),
+        panel.border = element_rect(colour = "black", size = 0.5))
+
 Prec_of_Wettest_Month_2011_2040_126_map <- ggplot(data = Prec_of_Wettest_Month_2011_2040_126_df, aes(x=Longitude, y=Latitude)) +
   geom_tile(aes(fill = `Prec_of_Wettest_Month_2011_2040_126`)) +
   labs(x = "Longitude", y = "Latitude") +
