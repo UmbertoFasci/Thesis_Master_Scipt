bd1fa58f4ec944c175f8ee984993933f0788fd12
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-11-06 21:58:36 -0600

    ANMN Current map

--- Scripts/Analysis_Plot_Groups_proj_onlyEnvs_FullExtent.R
+++ Scripts/Analysis_Plot_Groups_proj_onlyEnvs_FullExtent.R
@@ -541,6 +541,16 @@ MRIESM2_2041_2070_585_Histogram <- MRIESM2_4170_585_Stats %>% gghistogram(x = "P
 
 # Annual Mean Temp
 
+Annual_Mean_Temp_Current_map <- ggplot(data = Annual_Mean_Temperature_Curr_df, aes(x=Longitude, y=Latitude)) +
+  geom_tile(aes(fill = `Annual_Mean_Temperature_Current`)) +
+  labs(x = "Longitude", y = "Latitude") +
+  coord_sf(xlim = c(110.00, 160.00), ylim = c(-45.00, -10.00), expand = T, crs = "WGS84") +
+  annotation_scale(location = "bl", width_hint = 0.5) +
+  theme_bw() +
+  theme(legend.title = element_blank(),
+        legend.key.size = unit(6, "mm"), legend.text = element_text(size = 6),
+        panel.border = element_rect(colour = "black", size = 0.5))
+
 Annual_Mean_Temp_2011_2040_126_map <- ggplot(data = Annual_Mean_Temp_2011_2040_126_df, aes(x=Longitude, y=Latitude)) +
   geom_tile(aes(fill = `Annual_Mean_Temp_2011_2040_126`)) +
   labs(x = "Longitude", y = "Latitude") +
