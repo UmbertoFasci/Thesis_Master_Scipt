bf87f4d854d1360a64758b4cefc58aaf18c2b811
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-11-14 15:36:25 -0600

    remove size in place of color

--- Scripts/AUC_TSS_Plots.R
+++ Scripts/AUC_TSS_Plots.R
@@ -7,14 +7,14 @@ AUC_Results <- data.frame(AUC_Results)
 TSS_Results <- data.frame(TSS_Results)
 
 AUC_Plot <- ggplot(data = AUC_Results, aes(x = as.character(Variable.Group), y = Test.AUC)) +
-    geom_point(aes(size = Year.Range, shape = Background.Points)) +
+    geom_point(aes(color = Year.Range, shape = Background.Points)) +
     facet_wrap(~Projection) +
     geom_hline(yintercept = 0.8, linetype = 'dashed', color = 'red') +
     geom_jitter()
 
 
 TSS_Plot <- ggplot(data = TSS_Results, aes(x = as.character(Variable.Group), y = TSS)) +
-    geom_point(aes(size = Year.Range, shape = Background.Points)) +
+    geom_point(aes(color = Year.Range, shape = Background.Points)) +
     facet_wrap(~Projection) +
     geom_hline(yintercept = 0.4, linetype = 'dashed', color = 'red') +
     geom_jitter()
