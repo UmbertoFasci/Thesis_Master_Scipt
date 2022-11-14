360c1a7eca0584353dab6a748119dd0fd28a3c12
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-11-13 21:54:13 -0600

    Migrate Current

--- Scripts/TSS_script.R
+++ Scripts/TSS_script.R
@@ -1,3 +1,20 @@
+# Current
+
+TSS_Current_1k_vargroup1 <- Current_1k_vargroup1_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_10k_vargroup1 <- Current_10k_vargroup1_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_1k_vargroup2 <- Current_1k_vargroup2_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_10k_vargroup2 <- Current_10k_vargroup2_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_1k_vargroup3 <- Current_1k_vargroup3_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_10k_vargroup3 <- Current_10k_vargroup3_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_1k_vargroup4 <- Current_1k_vargroup4_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_10k_vargroup4 <- Current_10k_vargroup4_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_1k_vargroup5 <- Current_1k_vargroup5_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_10k_vargroup5 <- Current_10k_vargroup5_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_1k_vargroup6 <- Current_1k_vargroup6_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_10k_vargroup6 <- Current_10k_vargroup6_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_1k_vargroup7 <- Current_1k_vargroup7_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+TSS_Current_10k_vargroup7 <- Current_10k_vargroup7_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
+
 # MRIESM2_20112040_ssp126
 
 TSS_1140_126_1k_var1 <- MRIESM2_1140_ssp126_1k_vargroup1_results %>% select(contains('Maximum test sensitivity plus specificity Cloglog threshold'))
