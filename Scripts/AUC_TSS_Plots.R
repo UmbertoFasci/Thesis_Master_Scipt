8c2b7591b07f827aa3e4a62cfee730e214ab1695
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-11-14 15:36:55 -0600

    Remove jitter property

--- Scripts/AUC_TSS_Plots.R
+++ Scripts/AUC_TSS_Plots.R
@@ -9,12 +9,10 @@ TSS_Results <- data.frame(TSS_Results)
 AUC_Plot <- ggplot(data = AUC_Results, aes(x = as.character(Variable.Group), y = Test.AUC)) +
     geom_point(aes(color = Year.Range, shape = Background.Points)) +
     facet_wrap(~Projection) +
-    geom_hline(yintercept = 0.8, linetype = 'dashed', color = 'red') +
-    geom_jitter()
+    geom_hline(yintercept = 0.8, linetype = 'dashed', color = 'red')
 
 
 TSS_Plot <- ggplot(data = TSS_Results, aes(x = as.character(Variable.Group), y = TSS)) +
     geom_point(aes(color = Year.Range, shape = Background.Points)) +
     facet_wrap(~Projection) +
-    geom_hline(yintercept = 0.4, linetype = 'dashed', color = 'red') +
-    geom_jitter()
+    geom_hline(yintercept = 0.4, linetype = 'dashed', color = 'red')
