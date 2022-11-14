a201c1b3319e6b2373b6f8578d816459b2d2fcce
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-11-14 15:29:16 -0600

    update stat plots for model performance

--- Scripts/AUC_TSS_Plots.R
+++ Scripts/AUC_TSS_Plots.R
@@ -6,11 +6,13 @@ TSS_Results <- read.csv("~/Thesis_Master_Final/Thesis_Master_Script/Scripts/TSS_
 AUC_Results <- data.frame(AUC_Results)
 TSS_Results <- data.frame(TSS_Results)
 
-AUC_Plot <- ggplot(data = AUC_Results, aes(x = Variable.Group, y = Test.AUC)) +
+AUC_Plot <- ggplot(data = AUC_Results, aes(x = as.character(Variable.Group), y = Test.AUC)) +
     geom_point(aes(color = Year.Range, shape = Background.Points)) +
-    facet_wrap(~Projection)
+    facet_wrap(~Projection) +
+    geom_hline(yintercept = 0.8, linetype = 'dashed', color = 'red')
 
 
-TSS_Plot <- ggplot(data = TSS_Results, aes(x = Variable.Group, y = TSS)) +
+TSS_Plot <- ggplot(data = TSS_Results, aes(x = as.character(Variable.Group), y = TSS)) +
     geom_point(aes(color = Year.Range, shape = Background.Points)) +
-    facet_wrap(~Projection)
+    facet_wrap(~Projection) +
+    geom_hline(yintercept = 0.4, linetype = 'dashed', color = 'red')
