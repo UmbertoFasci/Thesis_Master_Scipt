23822457437aa40e7fb26895f5d020d11472a934
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-11-21 16:56:33 -0600

    fix human data 1140 585 (-128, 0)

--- Scripts/Analysis_Plot_Groups_proj_fullextent.R
+++ Scripts/Analysis_Plot_Groups_proj_fullextent.R
@@ -199,6 +199,12 @@ colnames(Mean_Temp_Driest_Qtr_2011_2040_585_df) <- c("Longitude", "Latitude", "M
 colnames(Prec_of_Wettest_Month_2011_2040_585_df) <- c("Longitude", "Latitude", "Prec_of_Wettest_Month_2011_2040_585")
 colnames(Human_Influence_2011_2040_585_df) <- c("Longitude", "Latitude", "Human_Influence_2011_2040_585")
 
+# Fix Human Data
+Human_Influence_2011_2040_585_df <- Human_Influence_2011_2040_585_df %>%
+  mutate(Human_Influence_2011_2040_585 = replace(Human_Influence_2011_2040_585,
+                                                 Human_Influence_2011_2040_585 < 0,
+                                                 0))
+
 # 2041-2070 ssp126
 
 colnames(Annual_Mean_Temp_2041_2070_126_df) <- c("Longitude", "Latitude", "Annual_Mean_Temp_2041_2070_126")
