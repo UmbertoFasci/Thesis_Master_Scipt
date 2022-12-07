8e4b4700f7d51d7aad1fda869a69ed58a028c309
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-12-06 23:55:51 -0600

    revert

--- Scripts/Analysis_Plot_Groups_proj_fullextent.R
+++ Scripts/Analysis_Plot_Groups_proj_fullextent.R
@@ -301,7 +301,6 @@ MRIESM2_4170_585_Stats <- MRIESM2_4170_585_Stats %>% mutate(Suitability_Percenti
 
 # Create percent bins (4 groups)
 
-# PErcent bins will be made here
 
 # Bin Comparisons for Kruskal-Wallis Test
 bin_comparisons <- list( c("1", "2"),
